aea64ee1499e64381c584f6727486f16040ea72a
unknown <stulli103@gmail.com>
2020-09-26 23:50:02 +0000

    Testing fastApi awesomeness

---
+++ main.py
@@ -0,0 +1,151 @@
+from typing import Optional, List
+
+from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel
+from models.item import Item
+from models.model_name import ModelName
+from functions.fake_items_db import fake_items_db
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return 'Hello world'
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    else:
+        item_dict.update({"price_with_tax": None})
+    return item_dict
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.post("/items/{item_id}")
+async def post_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+@app.get("/items/")
+async def read_items(
+    skip: int = 0, limit: int = 10, q: Optional[str] = Query(..., min_length=3, max_length=50)
+):
+    results = {"items": fake_items_db[skip: skip + limit]}
+    if q:
+        results.update({"q": q})
+
+    return results
+
+@app.get("/items/dict/")
+async def read_items_dict(
+    q: Optional[str] = Query(
+        None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/title/")
+async def read_items_title(
+    q: Optional[str] = Query(None, title="Query string", description="Some description", min_length=3)
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/list/")
+async def read_list_items(q: List[str] = Query(["foo", "bar"], min_length=3)):
+    return {"q": q}
+
+@app.get("/items/alias/")
+async def read_items_alias(q: Optional[str] = Query(None, alias="item-query")):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/deprecated_example/")
+async def read_items_deprecated_example(
+    q: Optional[str] = Query(
+        None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        deprecated=True,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/{item_id}/metadata/")
+async def read_items_metadata(
+    item_id: int = Path(..., title="The ID of the item to get"),
+    q: Optional[str] = Query(None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
---
+++ models/item.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
